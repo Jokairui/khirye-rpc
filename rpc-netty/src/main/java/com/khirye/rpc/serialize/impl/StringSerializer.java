237e042b9a1df280b6b7cea6c104fdfd2720dfee
zhou_kairui <zhou_kairui0101@wuxiapptec.com>
2020-01-05 15:03:05 +0800

    add the first Serializer implement ---- StringSerializer

--- rpc-netty/src/main/java/com/khirye/rpc/serialize/impl/StringSerializer.java
+++ rpc-netty/src/main/java/com/khirye/rpc/serialize/impl/StringSerializer.java
@@ -1,4 +1,33 @@
 package com.khirye.rpc.serialize.impl;
 
-public class StringSerializer {
+import com.khirye.rpc.serialize.Serializer;
+
+import java.nio.charset.StandardCharsets;
+
+public class StringSerializer implements Serializer<String> {
+    @Override
+    public int size(String entry) {
+        return entry.getBytes().length;
+    }
+
+    @Override
+    public void serialize(String entry, byte[] bytes, int offset, int length) {
+        byte[] stringBytes = entry.getBytes();
+        System.arraycopy(stringBytes, 0, bytes, offset, stringBytes.length);
+    }
+
+    @Override
+    public String parse(byte[] bytes, int offset, int length) {
+        return new String(bytes, offset, length, StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public byte type() {
+        return Types.TYPE_STRING;
+    }
+
+    @Override
+    public Class<String> getSerializeClass() {
+        return String.class;
+    }
 }
